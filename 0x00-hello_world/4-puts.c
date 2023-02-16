c9ac98824f77bb71787f1cd1b5020ee039550176
fidelis182 <fedymueni18@gmail.com>
2023-02-16 13:08:49 -0800

    error fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 /**
- * main -prints a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+ * main -prints "Programming is like building a multilingual puzzle,
  *
- * Return: the return value is 0
+ * Return: 0
  */
-int main(void)
-{
-        puts("\"Programming is like building a multilingual puzzle");
+ int main(void)
+{ 
+	 puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
