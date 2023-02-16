8a8b28295d19d6d2939ed6551929b352988b70d1
fidelis182 <fedymueni18@gmail.com>
2023-02-16 12:03:11 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include<stdio.h>
+
+/**
+ * main function prints
+ *"Programming is like building a multilingual puzzle"
+ */
+
 int main(void)
 {
 	char Mystr[] = "Programming is like building a multilingual puzzle";
