0210831c5de26b21f930a99e7b75322b97540bb1
fidelis182 <fedymueni18@gmail.com>
2023-02-16 13:03:52 -0800

    error fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,6 @@
-/**
- *file 4-puts.c
- *fidelis
- */
 #include<stdio.h>
 /**
- * main -prints Mystr
- * Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+ * main -prints a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
  *
  * Return: the return value is 0
  */
