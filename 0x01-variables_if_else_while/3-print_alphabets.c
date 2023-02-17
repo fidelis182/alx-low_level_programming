6352a2cfd63c8ea384c375055b36b55fbb516a3d
fidelis182 <fedymueni18@gmail.com>
2023-02-17 06:56:06 -0800

    error fix

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
  *main-entry point
- *Return: returns 0 
+ *Return:returns 0 
  *
  */
-int main()
+int main(void)
 {
-	char lowercase ='a';
-	char uppercase ='A';
+	char lowercase = 'a';
+	char uppercase = 'A';
 
-	while(lowercase <= 'z' )
+	while (lowercase <= 'z')
 	{
 		putchar(lowercase);
-		lowercase=lowercase+1;
+		lowercase++;
 	}
 	while (uppercase <= 'Z')
 	{
