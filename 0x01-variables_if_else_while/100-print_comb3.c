22714307d99ab31ab506bbd422dc1e112d640b0d
fidelis182 <fedymueni18@gmail.com>
2023-02-17 13:24:42 -0800

    100-print_combo3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main -entry point
+ *Retunr: return 0
+ */
+int main (void)
+{
+	int n;
+
+	for (n = 1; n < 100; n++)
+	{
+		putchar(n/10 + '0');
+		putchar(n%10 + '0');
+		if (n < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
