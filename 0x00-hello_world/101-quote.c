614d9101aa268cecd9ac2c48ce7a5e7c3216555e
fidelis182 <fedymueni18@gmail.com>
2023-02-19 23:50:59 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: return 0;
+ */
+int main(void)
+{
+	int n = "and that piece of art is useful" -" Dora Korpar, 2015-10-19";
+
+	putchar(n + '0');
+	return (1);
+}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@ int main(void)
 	int e;
 	long int b;
 	long long int k;
-	char c;
+	char c = sizeof(c);
 	float f;
 
-	printf("Size of char: %lu byte(s)\n", (unsigned long) sizeof(c));
+	printf("Size of char: %d byte(s)\n", (unsigned long) sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long) sizeof(e));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(k));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(b));
