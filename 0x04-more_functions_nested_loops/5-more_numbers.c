25be652154d111528c714cb0814c26c39c45ada2
fidelis182 <fedymueni18@gmail.com>
2023-02-23 04:12:29 -0800

    5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,19 +3,19 @@
 /**
  * more_numbers - writes the character void to stdout
  *
- * Return: no return value 
+ * Return: no return value
  */
 void more_numbers(void)
 {
 	int n, a;
 
-	for (n = 0; n < 10;n++)
+	for (n = 0; n < 10; n++)
 	{
 		for (a = 0; a <= 14; a++)
 		{
 			if (a >= 10)
 			{
-				_putchar('0' +a / 10);
+				_putchar('0' + a / 10);
 			}
 			_putchar('0' + a % 10);
 		}
