8e53cedfba364821d936b3841a9bfe0a022b44c7
fidelis182 <fedymueni18@gmail.com>
2023-02-24 11:53:13 -0800

    0-main.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- *positive_or_negative - assigns random number to the variable n
+ *positive_or_negative - assigns random number to the variable i
+ *@i: parameter to be checked
+ *
  *Return: always 0
  */
 void positive_or_negative(int i)
