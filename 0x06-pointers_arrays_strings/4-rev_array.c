050dbd6ee8393601f69bd8a163a3c0630bf3b065
fidelis182 <fedymueni18@gmail.com>
2023-03-02 12:42:16 -0800

    error init

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ *reverse_array - reverse the content of an array integer
+ *@a: character
+ *@n: number of elements in the array
+ *
+ *Return: nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int temp;
+	int i;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	}
+}
+
+/**
+ * print_array - check the code
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (i != 0)
+		{
+			printf(", ");
+		}
+		printf("%d", a[i]);
+		i++;
+	}
+	printf("\n");
+}
