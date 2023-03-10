692cfef67b10a332c4c45cd82a3c9abab5c800bb
fidelis182 <fedymueni18@gmail.com>
2023-03-10 04:32:57 -0800

    task 0

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,20 @@
+
+#include "main.h"
+
+/**
+ * _strlen - prints the length of the array
+ * @s: a pointer the character we want to get it`s length
+ *
+ * Return: length 'a'
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+
+	while (*s != '\0')
+	{
+		a++;
+		s++;
+	}
+	return (a);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @a: the int to check
+ * Return: the absolute value of int
+ */
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		return (-a);
+	}
+	else
+	{
+		return (a);
+	}
+}
