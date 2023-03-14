ba195a41f5657dfd570b01458b8c6faec6a653aa
fidelis182 <fedymueni18@gmail.com>
2023-03-14 13:36:58 -0700

    fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,9 +13,9 @@ char *str_concat(char *s1, char *s2)
 	char *ptr = (char *) malloc(count * sizeof(char));
 
 	if (s1 == NULL)
-		return (NULL);
+		s1 = "";
 	if (s2 == NULL)
-		return (NULL);
+		s2 = "";
 	strcpy(ptr, s1);
 	strcat(ptr, s2);
 	ptr[count - 1] = '\0';
