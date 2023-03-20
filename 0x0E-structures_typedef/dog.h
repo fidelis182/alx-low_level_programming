a35f573cadcdf8cd427cea6b13ca70bf5147e86c
fidelis182 <fedymueni18@gmail.com>
2023-03-20 09:16:39 -0700

    prints properties of a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog- defines dog elements
+ *@name: first element
+ *@owner: second element
+ *@age: third element
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
